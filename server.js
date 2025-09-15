c06bbf1235d125bfddb22783911bf5f825589e3f
garretts8 <gar21085@byui.edu>
2025-09-15 13:34:55 -0600

    Setup Github with CSE 341

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Serve static files from the current directory
+app.use(express.static(path.join(__dirname)));
+
+// Route for the root URL
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`Serving files from: ${__dirname}`);
+});
