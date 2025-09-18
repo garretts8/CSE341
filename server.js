28d276ed8094fba05a830fa0a08d11d1b2a7b6e8
garretts8 <gar21085@byui.edu>
2025-09-18 02:31:38 -0600

    Changed Howard to be the root browser

--- server.js
+++ server.js
@@ -3,21 +3,23 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Serve static files from the current directory
-app.use(express.static(path.join(__dirname)));
+
 
 // Route for the root URL
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
-});
-
-app.get('/Howard', (req, res) => {
     res.send('Howard Newman');
 });
 
 app.get('/Fabian', (req, res) => {
     res.send('Fabian Newman');
 });
+
+app.get('/index', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Serve static files from the current directory
+app.use(express.static(path.join(__dirname)));
 
 // Start the server
 app.listen(PORT, () => {
