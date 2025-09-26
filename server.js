5c78f05a33720b709746d47b2d5ae5f1e0af2f3f
garretts8 <gar21085@byui.edu>
2025-09-26 15:07:04 -0600

    Changed the name of the application site

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 var app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use('/', require('./routes/index.js'));
+app.use('/', require('./routes'));
 
 // Serve static files from the current directory
 app.use(express.static(path.join(__dirname)));
