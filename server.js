89869e4a52e3140a4fc12d7e58b03cfd46b603db
garretts8 <gar21085@byui.edu>
2026-01-08 18:07:52 -0700

    Created inital code for server.js, created folders to eventually separate code into models, route, controller folders

---
+++ server.js
@@ -0,0 +1,9 @@
+const  express = require("express");
+const app = express();
+app.get("/", (req, res) => {
+    res.send("Tommy Hilfigure");
+});
+
+const port = 3000;
+app.listen(process.env.port || port);
+console.log("Web Server is listening at port "+ (process.env.port || port));
