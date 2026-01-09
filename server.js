c7064a1cdf80a8e5fe384c99497844b9a0f7c5d7
garretts8 <gar21085@byui.edu>
2026-01-08 20:07:37 -0700

    Separated code from server.js to controller/lesson1.js file

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const  express = require("express");
 const app = express();
-app.get("/", (req, res) => {
-    res.send("Tommy Hilfigure");
-});
+const lesson1Controller = require("./controllers/lesson1");
+
+// Route
+app.get("/", lesson1Controller.emilyRoute);
+app.get("/hannah", lesson1Controller.hannahRoute);
+
 
 const port = 3000;
 app.listen(process.env.port || port);
